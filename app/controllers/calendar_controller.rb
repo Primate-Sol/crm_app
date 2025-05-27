fea2cd5267394649b6f3cd0916c402f4b6894448
Primate-Sol <lotzbattles@gmail.com>
2025-05-26 23:04:12 -0500

    Update calendar_controller.rb

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,5 +2,8 @@ class CalendarController < ApplicationController
   before_action :require_login
 
   def index
+    # This renders calendar/index.html.erb
+    # You can preload user-specific events here in the future, like:
+    # @events = JsonStorage.read("events.json").select { |e| e["user_id"] == current_user["id"] }
   end
 end
