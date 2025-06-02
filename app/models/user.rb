f869fedf385093da22ce23d36c25e722a7d1e7ee
Primate-Sol <lotzbattles@gmail.com>
2025-06-01 22:42:20 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,51 +1,44 @@
-require 'bcrypt'
-require 'json'
-require 'securerandom'
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
 
-class User
-  attr_accessor :id, :name, :email, :password_digest
-
-def initialize(attrs = {})
-  @id = attrs[:id] || attrs["id"] || SecureRandom.uuid
-  @name = attrs[:name] || attrs["name"]
-  @email = attrs[:email] || attrs["email"]
-  @password_digest = attrs[:password_digest] || attrs["password_digest"]
-end
-
-
-  def self.from_json(file_path)
-    return [] unless File.exist?(file_path)
-    JSON.parse(File.read(file_path)).map { |data| User.new(data) }
+  def new
+    # Render registration form
   end
 
-  def self.save_all(users, file_path)
-    File.write(file_path, JSON.pretty_generate(users.map(&:to_h)))
+  def create
+    file_path = Rails.root.join("data", "users.json")
+    users = User.from_json(file_path)
+
+    if users.any? { |u| u.email == params[:email] }
+      flash[:alert] = "Email already registered"
+      redirect_to register_path
+      return
+    end
+
+    password = params[:password]
+
+    unless valid_password?(password)
+      flash[:alert] = "Password must be at least 8 characters and include an uppercase letter, a lowercase letter, a number, and a special character."
+      redirect_to register_path
+      return
+    end
+
+    user = User.new(name: params[:name], email: params[:email])
+    user.password = password
+    users << user
+
+    User.save_all(users, file_path)
+    session[:user_id] = user.id
+    redirect_to clients_path, notice: "Registration successful!"
   end
 
-  def to_h
-    {
-      id: id,
-      name: name,
-      email: email,
-      password_digest: password_digest
-    }
-  end
+  private
 
-  def password=(new_password)
-    @password_digest = BCrypt::Password.create(new_password)
-  end
-
-  def authenticate(input_password)
-    return false unless password_digest
-    BCrypt::Password.new(password_digest) == input_password
-  rescue BCrypt::Errors::InvalidHash
-    false
-  end
-
-  # Helper to find a user by ID
-  def self.find_by_id(user_id)
-    users = from_json(Rails.root.join("data", "users.json"))
-    users.find { |u| u.id == user_id }
+  def valid_password?(password)
+    password.length >= 8 &&
+      password.match(/[A-Z]/) &&
+      password.match(/[a-z]/) &&
+      password.match(/[0-9]/) &&
+      password.match(/[^A-Za-z0-9]/)
   end
 end
-
