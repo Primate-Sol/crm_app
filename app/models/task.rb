751a391a73197e76cbd6017424b5181b5ceadf5d
Primate-Sol <lotzbattles@gmail.com>
2025-06-05 22:15:14 -0500

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,21 +1,26 @@
 require 'securerandom'
-require_relative '../../lib/json_encryption'
-require_relative '../../lib/input_sanitizer'
+require 'date'
+require_relative '../../lib/task_sanitizer'
 
 class Task
   include ActiveModel::Model
   attr_accessor :title, :description, :status, :due_date
   attr_reader :id
 
+  # Task statuses:
+  # - pending: Task is created but not started
+  # - in_progress: Task is currently being worked on
+  # - completed: Task is finished
   VALID_STATUSES = %w[pending in_progress completed]
 
   validates :title, presence: true
-  validates :status, inclusion: { in: VALID_STATUSES }, allow_nil: true
+  validates :status, inclusion: { in: VALID_STATUSES }
+  validates :due_date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: 'must be in ISO format (YYYY-MM-DD)' }, allow_nil: true
 
   def initialize(attributes = {})
-    super
+    sanitized = TaskSanitizer.sanitize(attributes)
+    super(sanitized)
     @id ||= SecureRandom.uuid
-    sanitize_inputs
   end
 
   def as_json(*)
@@ -45,17 +50,5 @@ class Task
   def self.save_all(tasks, file_path)
     data = tasks.map(&:as_json)
     JsonFileStore.write(file_path, data)
-  end
-
-  private
-
-  def sanitize_inputs
-    self.title = InputSanitizer.clean_string(title)
-    self.description = InputSanitizer.clean_string(description) if description
-    self.status = status if VALID_STATUSES.include?(status)
-    # Assuming due_date is a date or string in ISO format, sanitize if string:
-    if due_date.is_a?(String)
-      self.due_date = InputSanitizer.clean_string(due_date)
-    end
   end
 end
