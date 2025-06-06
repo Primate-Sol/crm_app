c4e481ba72ba6413e9d409d28a7077e4cd257872
Primate-Sol <lotzbattles@gmail.com>
2025-06-05 22:32:46 -0500

    Update client_sanitizer.rb

--- app/services/client_sanitizer.rb
+++ app/services/client_sanitizer.rb
@@ -1,15 +1,17 @@
+require_relative '../lib/input_sanitizer'
+
 class ClientSanitizer
   def self.sanitize(attributes)
-    @sanitizer = InputSanitizer
+    sanitizer = InputSanitizer
     {
-      name: @sanitizer.clean_string(attributes[:name]),
-      email: @sanitizer.normalize_email(attributes[:email]),
-      phone: sanitize_optional(attributes[:phone]),
-      notes: sanitize_optional(attributes[:notes])
+      name: sanitizer.clean_string(attributes[:name]),
+      email: sanitizer.normalize_email(attributes[:email]),
+      phone: sanitize_optional(attributes[:phone], sanitizer),
+      notes: sanitize_optional(attributes[:notes], sanitizer)
     }
   end
 
-  def self.sanitize_optional(value)
-    value ? @sanitizer.clean_string(value) : nil
+  def self.sanitize_optional(value, sanitizer)
+    value ? sanitizer.clean_string(value) : nil
   end
 end
