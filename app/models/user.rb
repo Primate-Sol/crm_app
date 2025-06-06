726cafbb25a9ca9cf73375c3be1fbe24043f7841
Primate-Sol <lotzbattles@gmail.com>
2025-06-05 20:33:32 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,52 @@
-class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
+class User
+  include ActiveModel::Model
+  attr_accessor :name, :email, :password
+  attr_reader :id
 
-  def new
-    # Render registration form
+  EMAIL_FORMAT = /\A[^@\s]+@[^@\s]+\z/o
+  PASSWORD_RULES = [
+    { regex: /.{8,}/, message: "must be at least 8 characters" },
+    { regex: /[a-z]/, message: "must include at least one lowercase letter" },
+    { regex: /[A-Z]/, message: "must include at least one uppercase letter" },
+    { regex: /\d/, message: "must include at least one number" },
+    { regex: /[^A-Za-z0-9]/, message: "must include at least one special character" }
+  ]
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: EMAIL_FORMAT }
+  validate :validate_password
+
+  def initialize(attributes = {})
+    super
+    @id ||= SecureRandom.uuid
   end
 
-  def create
-    file_path = Rails.root.join("data", "users.json")
-    users = User.from_json(file_path)
+  def password=(plain_password)
+    @password = plain_password
+    @password_digest = BCrypt::Password.create(plain_password)
+  end
 
-    if users.any? { |u| u.email == params[:email] }
-      flash[:alert] = "Email already registered"
-      redirect_to register_path
-      return
-    end
+  def as_json(*)
+    { id: id, name: name, email: email, password_digest: @password_digest }
+  end
 
-    password = params[:password]
+  def self.from_json(file_path)
+    # Load users from JSON file using JsonFileStore or similar
+  end
 
-    unless valid_password?(password)
-      flash[:alert] = "Password must be at least 8 characters and include an uppercase letter, a lowercase letter, a number, and a special character."
-      redirect_to register_path
-      return
-    end
-
-    user = User.new(name: params[:name], email: params[:email])
-    user.password = password
-    users << user
-
-    User.save_all(users, file_path)
-    session[:user_id] = user.id
-    redirect_to clients_path, notice: "Registration successful!"
+  def self.save_all(users, file_path)
+    # Save all users to JSON file
   end
 
   private
 
-  def valid_password?(password)
-    password.length >= 8 &&
-      password.match(/[A-Z]/) &&
-      password.match(/[a-z]/) &&
-      password.match(/[0-9]/) &&
-      password.match(/[^A-Za-z0-9]/)
+  def validate_password
+    if password.blank?
+      errors.add(:password, "can't be blank")
+    else
+      PASSWORD_RULES.each do |rule|
+        errors.add(:password, rule[:message]) unless password.match?(rule[:regex])
+      end
+    end
   end
 end
