590b316e7c4c7eb0af6a1c800e558dcf11cae491
Primate-Sol <lotzbattles@gmail.com>
2025-06-05 22:35:51 -0500

    Update task_sanitizer.rb

--- lib/task_sanitizer.rb
+++ lib/task_sanitizer.rb
@@ -1,20 +1,36 @@
-# lib/task_sanitizer.rb
+require_relative './input_sanitizer'
+
+# Optional: Define an interface module for clarity and safety
+module SanitizerStrategy
+  def clean_string(input)
+    raise NotImplementedError
+  end
+
+  def normalize_email(input)
+    raise NotImplementedError
+  end
+end
+
 class TaskSanitizer
+  STRING_ATTRS = [:title, :description, :status].freeze
+
   def initialize(sanitizer_strategy = InputSanitizer)
+    unless sanitizer_strategy.respond_to?(:clean_string)
+      raise ArgumentError, "Sanitizer must implement :clean_string"
+    end
+
     @sanitizer = sanitizer_strategy
   end
 
   def sanitize(attributes)
-    string_attrs = [:title, :description, :status]
-    sanitized_strings = batch_clean_strings(attributes.slice(*string_attrs))
-
+    sanitized_strings = batch_clean_strings(attributes.slice(*STRING_ATTRS))
     sanitized_strings.merge(due_date: sanitize_due_date(attributes[:due_date]))
   end
 
   private
 
-  def batch_clean_strings(attrs)
-    attrs.transform_values { |v| @sanitizer.clean_string(v) }
+  def batch_clean_strings(attr_hash)
+    attr_hash.transform_values { |v| @sanitizer.clean_string(v) }
   end
 
   def sanitize_due_date(raw_date)
